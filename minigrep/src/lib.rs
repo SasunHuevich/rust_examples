c6232d391ed5425484287f1381c41fd9ce9efd02
Ivan Petrov <ivanp6174@gmail.com>
2025-08-31 20:31:03 +0800

    improve(minigrep): realization of search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -17,7 +17,15 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) ->Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -28,9 +36,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-            Rust:
-            safe, fast, productive.
-            Pick there.";
+Rust:
+safe, fast, productive.
+Pick there.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
