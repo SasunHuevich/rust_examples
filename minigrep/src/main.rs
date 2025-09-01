8f00163f59034a85e55deab43a176f3772879397
11Samedi <ivanp6174@gmail.com>
2025-09-02 02:57:16 +0800

    refactor(minigrep): using iterators

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -21,9 +21,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
